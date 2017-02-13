8ca43a49a4285a2f4e0982ff4a9ec5769bdb776e
HiiYL <yonglian146@gmail.com>
2017-02-13 17:33:49 +0800

    Added googlenet

--- app.py
+++ app.py
@@ -5,6 +5,48 @@ import json
 import requests
 from flask import Flask, request
 
+import urllib
+import numpy as np
+import cv2
+import sys
+
+from googlenet import *
+
+if sys.version_info[0] == 3:
+    from urllib.request import urlopen
+else:
+    # Not Python 3 - today, it is most likely to be Python 2
+    # But note that this might need an update when Python 4
+    # might be around one day
+    from urllib import urlopen
+
+
+# METHOD #1: OpenCV, NumPy, and urllib
+def url_to_image(url):
+    # download the image, convert it to a NumPy array, and then read
+    # it into OpenCV format
+    resp = urlopen(url)
+    image = np.asarray(bytearray(resp.read()), dtype="uint8")
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+ 
+    # return the image
+    return image
+
+def evaluateImg(image):
+    width, height, _ = image.shape
+    image = image.astype(np.float32)
+    image_processed = process_image(cv2.resize(image,(224,224)))
+    out = model.predict(image_processed)
+
+    scores = out[0]
+    weights = np.array([1,2,3,4,5,6,7,8,9,10])
+    mean_score = (scores * weights).sum(axis=1)
+
+    return mean_score
+
+
+model = create_googlenet('googlenet_aesthetics_weights_distribution_2016-12-06 23:04:37.h5')
+
 app = Flask(__name__)
 
 
@@ -37,7 +79,13 @@ def webhook():
 
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
+                    # message_text = messaging_event["message"]["text"]  # the message's text
+                    if "attachments" in messaging_event["message"]:
+                        send_message(sender_id, "An image eh? I will be right on it, just give me a moment while i ...")
+                        image_url = messaging_event["message"]["attachments"][0]["payload"]["url"]
+                        image = url_to_image(image_url)
+                        score = evaluateImg(image)
+                        send_message(sender_id, "What a nice image, i have decided to rate it a %.1f"%score )
 
                     send_message(sender_id, "got it, thanks!")
 
@@ -78,9 +126,21 @@ def send_message(recipient_id, message_text):
 
 
 def log(message):  # simple wrapper for logging to stdout on heroku
-    print str(message)
+    print(str(message))
     sys.stdout.flush()
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
+
+
+                    # sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+                    # recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                    # if "text" in messaging_event["message"]:
+                    #     message_text = messaging_event["message"]["text"]  # the message's text
+                    #     send_message(sender_id, "got it ( a text ), thanks!")
+                    # elif "attachments" in messaging_event["message"]:
+                    #     image_url = messaging_event["message"]["attachments"][0]["payload"]["url"]
+                    #     print(image_url)
+                    # else:
+                    #     send_message(sender_id, "got it ( unknown ), thanks!")
